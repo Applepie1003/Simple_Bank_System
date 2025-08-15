b0c6d1bd34c669a0cf3c92336da4e01adb6d3ea7
Park Junoh <105327444+Applepie1003@users.noreply.github.com>
2025-08-15 04:50:46 +0000

    create class

---
+++ Bank.java
@@ -0,0 +1,18 @@
+public class Bank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /*
+         * 사용자 인테페이스
+         * 1. 계좌생성
+         * 2. 출금
+         * 3. 입금
+         * 4. 잔액조회
+         * 5. 거래내역
+         * 6. 종료
+         */
+        while(1) {
+            
+        }
+    }
+}
